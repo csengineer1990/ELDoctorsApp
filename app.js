2717e0dbfa9337b3b402cb7ca80000f1ecd78687
csengineer1990 <68468678+csengineer1990@users.noreply.github.com>
2023-01-29 20:55:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const patientRoutes = require("./routes/patientRoutes");
+const cookieParser = require("cookie-parser");
+const registerRoute = require("./routes/registerRoute");
+const doctorRoute = require("./routes/doctorRoute");
+const adminRoutes = require("./routes/adminRoutes");
+const logoutRoute = require("./routes/logoutRoute");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+dotenv.config({ path: "./config.env" });
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+const dbURI = process.env.DATABASE;
+const port = process.env.PORT || 5000;
+
+// // Middlewares
+// app.use(express.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "https://easylife-healthplanet.onrender.com",
+//     ],
+//     credentials: true,
+//   })
+// );
+
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    app.listen(port);
+    console.log("connected to db and listening at port " + port);
+  })
+  .catch((err) => {
+    app.listen(port);
+    app.get("/", (req, res) => {
+      res.send(
+        "Something Went Wrong! Please Try again after some time, if problem persists please contact us."
+      );
+    });
+  });
+
+app.use(authRoutes);
+app.use(registerRoute);
+app.use(doctorRoute);
+app.use(patientRoutes);
+app.use(adminRoutes);
+app.use(logoutRoute);
+
+//Static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static("client/build"));
+//   const path = require("path");
+//   app.get("*", function (req, res) {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
